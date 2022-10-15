45ec287598783b70324e37884606c650bfbc2555
Ngo Jun Hao Jason <njhjason@protonmail.com>
2022-10-15 23:32:03 +0800

    Slice linked list

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Union
 
 
 class LinkedList:
@@ -14,23 +15,66 @@ class LinkedList:
 
             self.length += 1
 
-    def __getitem__(self, index: int) -> int:
+    def __getitem__(self, subscript: Union[int, slice]) -> Union[int, LinkedList]:
+
+        if isinstance(subscript, int):
+            return self._get_value(subscript)
+        elif isinstance(subscript, slice):
+            return self._get_slice(subscript)
+        else:
+            raise TypeError
+
+    def _get_value(self, subscript: int) -> int:
         if self.head_node is None:
             raise IndexError
 
-        if index == 0:
-            return self.head_node.value
+        index = self._make_index_positive(subscript)
+
+        if index >= self.length:
+            raise IndexError
 
         current_node = self.head_node
 
         for _ in range(index):
             current_node = current_node.next_node
 
-            if current_node is None:
-                raise IndexError
-
         return current_node.value
-    
+
+    def _get_slice(self, subscript: slice) -> LinkedList:
+        start, stop, step = subscript.start, subscript.stop, subscript.step
+
+        start = self._make_index_positive(start)
+        stop = self._make_index_positive(stop)
+        step = 1 if step is None else step
+
+        if step > 0:
+            if start >= self.length:
+                return LinkedList()
+
+            current_node = self.head_node
+
+            for _ in range(start):
+                current_node = current_node.next_node
+
+            values = []  # TODO: remove intermediate data structure
+            sublist_length = stop - start
+
+            for _ in range(0, sublist_length):
+                values.append(current_node.value)
+                current_node = current_node.next_node
+
+            return LinkedList(*values)
+
+        elif step < 0:
+            if stop >= self.length:
+                return LinkedList()
+            pass
+        else:
+            raise ValueError
+
+    def _make_index_positive(self, index: int) -> int:
+        return self.length + index if index < 0 else index
+
     def __len__(self) -> int:
         return self.length
 
--- linked_list/test_linked_list.py
+++ linked_list/test_linked_list.py
@@ -5,31 +5,22 @@ from timeit import timeit
 from .linked_list import LinkedList
 
 
-def test_create_linked_list_from_empty_array():
+def test_create_empty_linked_list():
     linked_list = LinkedList()
 
     assert linked_list is not None
     assert linked_list.head_node is None
 
 
-def test_create_linked_list_from_one_value():
-    value = 2
-    linked_list = LinkedList(value)
-
-    assert linked_list is not None
-    assert linked_list.head_node is not None
-    assert linked_list.head_node.value == value
-
-
-def test_create_linked_list_from_multiple_values():
-    values = [2, 3, 5, 7, 11]
-    linked_list = LinkedList(*values)
+def test_create_linked_list_from_given_array():
+    array = [2, 3, 5, 7, 11]
+    linked_list = LinkedList(*array)
 
     assert linked_list is not None
 
     current_node = linked_list.head_node
 
-    for value in values:
+    for value in array:
         assert current_node is not None
         assert current_node.value == value
 
@@ -38,33 +29,48 @@ def test_create_linked_list_from_multiple_values():
     assert current_node is None  # reached the end
 
 
-def test_get_item_from_empty_linked_list_raises_error():
+def test_get_value_from_empty_linked_list_raises_error():
     linked_list = LinkedList()
 
     with pytest.raises(IndexError):
         linked_list[0]
 
 
-def test_get_item_from_linked_list_with_one_value_by_index():
-    value = 2
-    linked_list = LinkedList(value)
+def test_get_value_from_linked_list_with_index_beyond_range():
+    linked_list = LinkedList(0)
 
-    assert linked_list[0] == value
+    with pytest.raises(IndexError):
+        linked_list[1]
 
 
-def test_get_item_from_linked_list_with_multiple_values_by_positive_indices():
-    values = [2, 3, 5, 7, 11]
-    linked_list = LinkedList(*values)
+def test_get_value_from_linked_list_with_invalid_index_type():
+    linked_list = LinkedList(0)
 
-    for index in range(len(values)):
-        assert linked_list[index] == values[index]
+    with pytest.raises(TypeError):
+        linked_list["A"]
+
+
+def test_get_value_from_linked_list_by_positive_indices():
+    array = [2, 3, 5, 7, 11]
+    linked_list = LinkedList(*array)
+
+    for index in range(len(array)):
+        assert linked_list[index] == array[index]
+
+
+def test_get_value_from_linked_list_by_negative_indices():
+    array = [2, 3, 5, 7, 11]
+    linked_list = LinkedList(*array)
+
+    for index in range(-1, -len(array) - 1, -1):
+        assert linked_list[index] == array[index]
 
 
 def test_get_length_of_linked_list():
-    values = [2, 3, 5, 7, 11]
-    linked_list = LinkedList(*values)
+    array = [2, 3, 5, 7, 11]
+    linked_list = LinkedList(*array)
 
-    assert len(linked_list) == len(values)
+    assert len(linked_list) == len(array)
 
 
 def test_get_length_of_linked_list_in_constant_time():
@@ -74,19 +80,48 @@ def test_get_length_of_linked_list_in_constant_time():
     time_taken_for_small = timeit(lambda: len(small_linked_list))
     time_taken_for_big = timeit(lambda: len(big_linked_list))
 
-    assert time_taken_for_small == pytest.approx(time_taken_for_big, 0.01)
+    assert time_taken_for_small == pytest.approx(time_taken_for_big, 0.1)
 
 
 def test_iterate_over_empty_linked_list():
     linked_list = LinkedList()
-    values = [value for value in linked_list]
+    array = [value for value in linked_list]
 
-    assert not values
+    assert not array
 
 
-def test_iterate_over_linked_list_with_multiple_values():
-    values = [2, 3, 5, 7, 11]
-    linked_list = LinkedList(*values)
+def test_iterate_over_linked_list():
+    array = [2, 3, 5, 7, 11]
+    linked_list = LinkedList(*array)
 
-    for linked_list_value, array_value in zip(linked_list, values):
+    for linked_list_value, array_value in zip(linked_list, array):
         assert linked_list_value == array_value
+
+
+def test_reverse_linked_list():
+    array = [2, 3, 5, 7, 11]
+    linked_list = LinkedList(*array)
+
+    for linked_list_value, array_value in zip(reversed(linked_list), reversed(array)):
+        assert linked_list_value == array_value
+
+
+def test_slice_linked_list():
+    array = [2, 3, 5, 7, 11]
+    linked_list = LinkedList(*array)
+
+    subarray = array[1:3]
+    sublist = linked_list[1:3]
+
+    assert len(sublist) == len(subarray)
+
+    for linked_list_value, array_value in zip(sublist, subarray):
+        assert linked_list_value == array_value
+
+
+# TODO
+# slice step more than 1
+# negative slice step
+# insert()
+# pop()
+# remove()
