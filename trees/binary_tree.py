1f3fe09c295895e9d6cde252de51ddd4018bd832
Ngo Jun Hao Jason <njhjason@protonmail.com>
2022-10-16 21:42:05 +0800

    Create and traverse binary tree

---
+++ trees/binary_tree.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+from enum import Enum
+
+
+class Traversal(Enum):
+    IN_ORDER = 0
+    PRE_ORDER = 1
+    POST_ORDER = 2
+
+
+class BinaryTree:
+    def __init__(self, *values) -> None:
+        if not values:
+            raise ValueError
+
+        self.root = TreeNode(values[0])
+
+        node_queue = [self.root]
+
+        for value in values[1:]:
+            new_node = TreeNode(value)
+
+            root_node = node_queue.pop(0)
+
+            if root_node.left is None:
+                root_node.left = new_node
+                node_queue.insert(0, root_node)
+
+            elif root_node.right is None:
+                root_node.right = new_node
+
+            else:
+                raise ValueError
+
+            node_queue.append(new_node)
+
+    def to_list(self, traversal: Traversal = Traversal.IN_ORDER) -> list[int]:
+        result = []
+
+        if traversal == Traversal.IN_ORDER:
+            result = self._traverse_in_order(self.root, result)
+
+        elif traversal == Traversal.PRE_ORDER:
+            result = self._traverse_pre_order(self.root, result)
+
+        elif traversal == Traversal.POST_ORDER:
+            result = self._traverse_post_order(self.root, result)
+
+        else:
+            raise ValueError
+
+        return result
+
+    def _traverse_in_order(self, tree_node: TreeNode, result: list[int]) -> list[int]:
+        if tree_node is None:
+            return result
+
+        result = self._traverse_in_order(tree_node.left, result)
+        result.append(tree_node.value)
+        result = self._traverse_in_order(tree_node.right, result)
+
+        return result
+
+    def _traverse_pre_order(self, tree_node: TreeNode, result: list[int]) -> list[int]:
+        if tree_node is None:
+            return result
+
+        result.append(tree_node.value)
+        result = self._traverse_pre_order(tree_node.left, result)
+        result = self._traverse_pre_order(tree_node.right, result)
+
+        return result
+
+    def _traverse_post_order(self, tree_node: TreeNode, result: list[int]) -> list[int]:
+        if tree_node is None:
+            return result
+
+        result = self._traverse_post_order(tree_node.left, result)
+        result = self._traverse_post_order(tree_node.right, result)
+        result.append(tree_node.value)
+
+        return result
+
+
+class TreeNode:
+    def __init__(
+        self,
+        value: int,
+        left: TreeNode = None,
+        right: TreeNode = None,
+    ) -> None:
+        self.value = value
+        self.left = left
+        self.right = right
---
+++ trees/test_binary_tree.py
@@ -0,0 +1,44 @@
+import pytest
+
+from .binary_tree import BinaryTree, Traversal
+
+
+def test_create_empty_binary_tree():
+    with pytest.raises(ValueError):
+        BinaryTree()
+
+
+def test_traverse_binary_tree_in_order():
+    expected_result = [7, 3, 11, 2, 13, 5, 17]
+
+    binary_tree = BinaryTree(2, 3, 5, 7, 11, 13, 17)
+    actual_result = binary_tree.to_list()
+
+    assert len(actual_result) == len(expected_result)
+
+    for actual_value, expected_value in zip(actual_result, expected_result):
+        assert actual_value == expected_value
+
+
+def test_traverse_binary_tree_pre_order():
+    expected_result = [2, 3, 7, 11, 5, 13, 17]
+
+    binary_tree = BinaryTree(2, 3, 5, 7, 11, 13, 17)
+    actual_result = binary_tree.to_list(Traversal.PRE_ORDER)
+
+    assert len(actual_result) == len(expected_result)
+
+    for actual_value, expected_value in zip(actual_result, expected_result):
+        assert actual_value == expected_value
+
+
+def test_traverse_binary_tree_post_order():
+    expected_result = [7, 11, 3, 13, 17, 5, 2]
+
+    binary_tree = BinaryTree(2, 3, 5, 7, 11, 13, 17)
+    actual_result = binary_tree.to_list(Traversal.POST_ORDER)
+
+    assert len(actual_result) == len(expected_result)
+
+    for actual_value, expected_value in zip(actual_result, expected_result):
+        assert actual_value == expected_value
