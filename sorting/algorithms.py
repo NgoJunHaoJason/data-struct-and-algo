74e183b7755b992a3af37ccedb211687f2dfa456
Ngo Jun Hao Jason <njhjason@protonmail.com>
2022-10-20 22:43:14 +0800

    Quick sort

--- sorting/algorithms.py
+++ sorting/algorithms.py
@@ -12,7 +12,7 @@ def insertion_sort(array: list[int]) -> None:
 
 
 def merge_sort(array: list[int]) -> None:
-    _merge_sort(array, 0, len(array) - 1)
+    _merge_sort(array, start=0, end=len(array) - 1)
 
 
 def _merge_sort(array: list[int], start: int, end: int) -> None:
@@ -67,3 +67,33 @@ def _merge(
 
     else:
         raise RuntimeError
+
+
+def quick_sort(array: list[int]) -> None:
+    _quick_sort(array, start=0, end=len(array) - 1)
+
+
+def _quick_sort(array: list[int], start: int, end: int) -> None:
+    if start >= end:
+        return
+
+    pivot_index = _partition(array, start, end)
+
+    _quick_sort(array, start=start, end=pivot_index - 1)
+    _quick_sort(array, start=pivot_index + 1, end=end)
+
+
+def _partition(array: list[int], start: int, end: int) -> int:
+    pivot_index, pivot_value = start, array[start]
+
+    for index in range(start + 1, end + 1):
+        if array[index] < pivot_value:
+            pivot_index += 1
+            _swap(array, pivot_index, index)
+
+    _swap(array, start, pivot_index)
+    return pivot_index
+
+
+def _swap(array: list[int], index1: int, index2: int) -> None:
+    array[index1], array[index2] = array[index2], array[index1]
--- sorting/test_algorithms.py
+++ sorting/test_algorithms.py
@@ -1,7 +1,7 @@
 import random
 
 
-from .algorithms import insertion_sort, merge_sort
+from .algorithms import insertion_sort, merge_sort, quick_sort
 
 
 def test_insertion_sort_on_empty_array():
@@ -53,6 +53,34 @@ def test_merge_sort_on_small_array():
     random.shuffle(actual_array)
 
     merge_sort(actual_array)
+
+    assert len(actual_array) == len(expected_array)
+
+    for actual_value, expected_value in zip(actual_array, expected_array):
+        assert actual_value == expected_value
+
+
+def test_quick_sort_on_empty_array():
+    expected_array = []
+    actual_array = expected_array[:]
+
+    random.shuffle(actual_array)
+
+    quick_sort(actual_array)
+
+    assert len(actual_array) == len(expected_array)
+
+    for actual_value, expected_value in zip(actual_array, expected_array):
+        assert actual_value == expected_value
+
+
+def test_quick_sort_on_small_array():
+    expected_array = [number for number in range(2**3)]
+    actual_array = expected_array[:]
+
+    random.shuffle(actual_array)
+
+    quick_sort(actual_array)
 
     assert len(actual_array) == len(expected_array)
 
