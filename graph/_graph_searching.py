623554e850d13fee0e3f9ed35997a18a3f00a290
Ngo Jun Hao Jason <njhjason@protonmail.com>
2022-10-22 20:29:55 +0800

    Depth-first search

--- graph/_graph_searching.py
+++ graph/_graph_searching.py
@@ -30,3 +30,30 @@ def breadth_first_search(
         )
 
     return None
+
+
+def depth_first_search(
+    directed_graph: DirectedGraph,
+    start_vertex: Vertex,
+    search_value: int,
+) -> Optional[Vertex]:
+    visited_vertices = set()
+    vertices_to_visit = [start_vertex]
+
+    while vertices_to_visit:
+        vertex = vertices_to_visit.pop(-1)
+
+        if vertex.value == search_value:
+            return vertex
+
+        visited_vertices.add(vertex)
+
+        vertices_to_visit.extend(
+            [
+                edge.to_vertex
+                for edge in directed_graph.adjacency_map[vertex]
+                if edge.to_vertex not in visited_vertices
+            ]
+        )
+
+    return None
