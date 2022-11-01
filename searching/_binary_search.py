ddfef9029154bbc0a53c30b210c2c662dd475b06
Ngo Jun Hao Jason <njhjason@protonmail.com>
2022-11-01 23:50:22 +0800

    Analyse complexity of searching algorithms

--- searching/_binary_search.py
+++ searching/_binary_search.py
@@ -1,21 +1,48 @@
-def binary_search(array: list[int], target_value: int) -> int:
+def binary_search(sorted_array: list[int], target_value: int) -> int:
+    """Search for the target value in a sorted array of size n,
+    checking only half of the array in subsequent invocations
+
+    Analysis
+    --------
+    - worst case: value is not in array, or is last element of array
+    - time complexity: O(log n)
+    - additional data structures used: None
+    - space complexity: O(1)
+
+    Parameters
+    ----------
+    array : list[int]
+        The array to search in
+    target_value : int
+        The value to search for
+
+    Returns
+    -------
+    int
+        the index of the value if found (-1 if not found)
+    """
     return (
         -1
-        if len(array) == 0
-        else _binary_search(array, target_value, 0, len(array) - 1)
+        if len(sorted_array) == 0
+        else _binary_search(sorted_array, target_value, 0, len(sorted_array) - 1)
     )
 
 
-def _binary_search(array: list[int], target_value: int, start: int, end: int) -> int:
+def _binary_search(
+    sorted_array: list[int],
+    target_value: int,
+    start: int,
+    end: int,
+) -> int:
     if start >= end:
-        return start if array[start] == target_value else -1
+        return start if sorted_array[start] == target_value else -1
 
     middle = (start + end) // 2
-    middle_value = array[middle]
+    middle_value = sorted_array[middle]
 
     if middle_value > target_value:
-        return _binary_search(array, target_value, start, middle - 1)
+        return _binary_search(sorted_array, target_value, start, middle - 1)
     elif middle_value < target_value:
-        return _binary_search(array, target_value, middle + 1, end)
+        return _binary_search(sorted_array, target_value, middle + 1, end)
     else:
         return middle_value
--- searching/_linear_search.py
+++ searching/_linear_search.py
@@ -1,4 +1,25 @@
 def linear_search(array: list[int], target_value: int) -> int:
+    """Search for the target value in an array of size n, in sequence
+
+    Analysis
+    --------
+    - worst case: value is not in array, or is last element of array
+    - time complexity: O(n)
+    - additional data structures used: None
+    - space complexity: O(1)
+
+    Parameters
+    ----------
+    array : list[int]
+        The array to search in
+    target_value : int
+        The value to search for
+
+    Returns
+    -------
+    int
+        the index of the first occurrence of the value if found (-1 if not found)
+    """
     for index, value in enumerate(array):
         if value == target_value:
             return index
